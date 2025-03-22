af5f4eef96459d1eb4faee482549e84c90142cfe
minipog <62334305+minipog@users.noreply.github.com>
2025-03-22 16:59:02 +0100

    Refactor: seperate Locations and LocationCard

---
+++ src/renderer/src/components/Locations/LocationCard.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+
+export default function LocationCard({ ...props }) {
+  return (
+    <Card body>
+      {props.name}
+      <p className="text-muted">
+        <small>
+          {props.address}
+          <br />
+          Opened: {new Date(props.openingDate).toDateString()}
+        </small>
+      </p>
+      <Link to={`/locations/${props._id}`}>
+        <Button variant="outline-secondary" className="float-end">
+          View
+        </Button>
+      </Link>
+    </Card>
+  )
+}
--- src/renderer/src/components/Locations/Locations.jsx
+++ src/renderer/src/components/Locations/Locations.jsx
@@ -1,7 +1,6 @@
-import { useLoaderData, Link } from 'react-router'
+import { useLoaderData } from 'react-router'
 import Stack from 'react-bootstrap/Stack'
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
+import LocationCard from './LocationCard'
 
 // eslint-disable-next-line react-refresh/only-export-components
 export async function locationLoader() {
@@ -20,14 +19,7 @@ export default function Locations() {
   return (
     <Stack gap={3} className="me-2">
       {locations.map((location, i) => (
-        <Card body key={i}>
-          {location.name} <small className="text-muted">({location.address})</small>
-          <Link to={`/locations/${location._id}`}>
-            <Button variant="light" className="float-end">
-              View
-            </Button>
-          </Link>
-        </Card>
+        <LocationCard key={i} {...location} />
       ))}
     </Stack>
   )

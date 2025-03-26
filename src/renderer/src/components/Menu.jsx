c477f6bbd581fc71f85e9ff39b720d47dda21097
minipog <62334305+minipog@users.noreply.github.com>
2025-03-26 13:20:12 +0100

    Menu cosmetics

--- src/renderer/src/components/Menu.jsx
+++ src/renderer/src/components/Menu.jsx
@@ -4,22 +4,29 @@ import { NavLink } from 'react-router'
 import Loading from './Loading'
 
 function Menu() {
-  const links = [{ label: 'Dashboard', path: '/' }, 'Locations', 'Machines', 'Inventory', 'Notes']
+  const links = [
+    { icon: '🖳', label: 'Dashboard', path: '/' },
+    { icon: '⌸', label: 'Locations', path: '/locations' },
+    { icon: '⌻', label: 'Machines', path: '/machines' },
+    { icon: '🖹', label: 'Inventory', path: '/inventory' },
+    { icon: '🗋', label: 'Notes', path: '/notes' }
+  ]
 
   return (
     <Container className="menu">
       <Nav defaultActiveKey={0} className="flex-column">
         {links.map((link, i) => (
-          <NavLink key={i} to={link.path || `/${link.toLowerCase()}`}>
+          <NavLink key={i} to={link.path}>
             {({ isPending }) => (
-              <>
-                {link.label || link}
+              <div className="d-flex p-2 gap-2">
+                <span className="fs-1">{link.icon}</span>
+                <span className="fw-semibold">{link.label}</span>
                 {isPending && (
-                  <span className="float-end">
+                  <span className="ms-auto">
                     <Loading animation="border" size="sm" />
                   </span>
                 )}
-              </>
+              </div>
             )}
           </NavLink>
         ))}
